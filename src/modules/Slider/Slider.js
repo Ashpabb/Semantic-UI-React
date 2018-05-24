57392b29ae57a358ac211e4a989784b82c5bc67e
Ashley Abbott <labbott@carbonite.com>
2018-05-24 14:08:31 -0400

    feat(slider): Most functionality complete. Proper Init commit
    This is most of the functionality built, although a tad ugly. Missing some props still like vertical, labels, disabled, hover handles, colors, sizes, inverted, basic

--- src/modules/Slider/Slider.js
+++ src/modules/Slider/Slider.js
@@ -22,6 +22,8 @@ const _meta = {
   type: META.TYPES.MODULE,
 }
 
+const precision = 4
+
 const debug = makeDebugger('slider')
 
 /**
@@ -34,12 +36,12 @@ const LOWER_BOUND = 'lowerBound'
  * Utility Functions
  */
 /** from mdn => /JavaScript/Reference/Global_Objects/Math/round#A_better_solution */
-function precisionRound(number, precision) {
+function precisionRound(n, p = precision) {
   const shift = (num, exponent) => {
     const numArray = `${num}`.split('e')
     return +`${numArray[0]}e${numArray[1] ? +numArray[1] + exponent : exponent}`
   }
-  return shift(Math.round(shift(number, +precision)), -precision)
+  return shift(Math.round(shift(n, +p)), -p)
 }
 
 function constrainNum(num, min, max) {
@@ -48,8 +50,28 @@ function constrainNum(num, min, max) {
   return num
 }
 
-function formatPctStr(num) {
-  return `${num}%`
+function findNumDigits(num, count, max = precision) {
+  const c = count || 0
+  if (!!max && max < c + 1) {
+    return false
+  }
+  if ((num * 10 ** c) % 1 !== 0) {
+    return findNumDigits(num, c + 1, max)
+  }
+  return c
+}
+
+// Javascript has error trying to mod using floats. This is a workaround up to X decimal places.
+function floatModDiv(dividend, divisor) {
+  const pad = 10 ** Math.max(findNumDigits(dividend), findNumDigits(divisor))
+  if ((divisor * pad) % 1 !== 0) {
+    throw new Error('There was an unexpected math error with Slider')
+  }
+  return ((dividend * pad) % (divisor * pad)) / pad
+}
+
+function invertPct(num) {
+  return 100 - num
 }
 
 /**
@@ -66,12 +88,6 @@ export default class Slider extends Component {
     /** A slider can appear disabled and be unable to change states */
     disabled: PropTypes.bool,
 
-    /** A slider can have two handles and select a start and doubleStart */
-    doubled: PropTypes.bool,
-
-    /** The start value for the second handle in a doubled slider */
-    doubleStart: PropTypes.number,
-
     /** Removes padding for a label. Auto applied when there is no label. */
     fitted: PropTypes.bool,
 
@@ -80,6 +96,8 @@ export default class Slider extends Component {
 
     /** The text of the associated label element. */
     label: customPropTypes.itemShorthand,
+
+    lower: PropTypes.number,
 
     /** The maximum value for the slider range */
     max: PropTypes.number,
@@ -114,17 +132,84 @@ export default class Slider extends Component {
      */
     onMouseDown: PropTypes.func,
 
-    /** Precision for rounding - likely never set */
-    precision: PropTypes.number,
+    upper: PropTypes.number,
 
-    /** The handles can lock to the step location onMove or they can be smooth */
+    /** If reversed, order values from greatest to lowest */
+    reversed: PropTypes.bool,
+
+    /** Dragging handles can lock to the increment or they can be smooth */
     smooth: PropTypes.bool,
 
-    /** The start value for the primary handle in a slider */
-    start: PropTypes.number,
+    /** The start value for the lower handle in a slider */
+    lowerStart: (props, propName, componentName) => {
+      const prop = props[propName]
+      if (!prop) {
+        if (!props.upperStart) {
+          return new Error(
+            `Invalid prop ${propName} supplied to ${componentName}. ${propName} is conditionally required.`,
+          )
+        }
+      } else {
+        if (_.isNumber(props.max) && prop > props.max) {
+          return new Error(
+            `Invalid prop ${propName} supplied to ${componentName}. ${propName} cannot exceed prop "max".`,
+          )
+        }
+        if (props.upperStart && _.isNumber(props.upperStart) && prop > props.upperStart) {
+          return new Error(
+            `Invalid prop ${propName} supplied to ${componentName}. prop upperStart cannot exceed ${propName}.`,
+          )
+        }
+        if (_.isNumber(props.min) && prop < props.min) {
+          return new Error(
+            `Invalid prop ${propName} supplied to ${componentName}. prop "min" cannot exceed prop ${propName}.`,
+          )
+        }
+      }
+    },
 
-    /** The value each slider step */
-    step: PropTypes.number,
+    /** The start value for the upper handle in a slider */
+    upperStart: (props, propName, componentName) => {
+      const prop = props[propName]
+      if (!prop) {
+        if (!props.lowerStart) {
+          return new Error(
+            `Invalid prop ${propName} supplied to ${componentName}. ${propName} is conditionally required.`,
+          )
+        }
+      } else {
+        if (_.isNumber(props.max) && prop > props.max) {
+          return new Error(
+            `Invalid prop ${propName} supplied to ${componentName}. ${propName} cannot exceed prop "max".`,
+          )
+        }
+        if (props.lowerStart && _.isNumber(props.lowerStart) && prop < props.lowerStart) {
+          return new Error(
+            `Invalid prop ${propName} supplied to ${componentName}. prop lowerStart cannot exceed ${propName}.`,
+          )
+        }
+        if (_.isNumber(props.min) && prop < props.min) {
+          return new Error(
+            `Invalid prop ${propName} supplied to ${componentName}. prop "min" cannot exceed prop ${propName}.`,
+          )
+        }
+      }
+    },
+
+    /** The value each slider increment. 0 will return real values. */
+    increment: (props, propName, componentName) => {
+      const prop = props[propName]
+      if (!_.isNumber(prop)) {
+        return new Error(
+          `Invalid prop ${propName} supplied to ${componentName}. Must be of type: Number`,
+        )
+      }
+      if (findNumDigits(prop) === false) {
+        return new Error(
+          `Invalid prop ${propName} supplied to ${componentName}. Number is too complicated`,
+        )
+      }
+    },
 
     /** A slider can receive focus. */
     tabIndex: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
@@ -135,12 +220,9 @@ export default class Slider extends Component {
 
   static defaultProps = {
     min: 0,
-    max: 20,
-    step: 1,
-    start: 1,
-    doubleStart: 0,
+    max: 10,
+    increment: 1,
     smooth: false,
-    precision: 1,
   }
 
   static autoControlledProps = [
@@ -151,28 +233,30 @@ export default class Slider extends Component {
 
   constructor(props) {
     super(props)
-    const { start, doubleStart } = this.props
+
+    const { min, max, lowerStart, upperStart } = this.props
+
     this.state = {
-      lowerVal: doubleStart,
-      upperVal: start,
+      lowerRoundVal: lowerStart ? this.roundToIncrement(lowerStart) : min,
+      upperRoundVal: upperStart ? this.roundToIncrement(upperStart) : max,
     }
   }
 
   onStart = () => {
-    const { upperVal, lowerVal } = this.state
+    const { upperRoundVal, lowerRoundVal } = this.state
     debug('startDrag')
     this.setState({
-      ogLowerVal: lowerVal,
-      ogUpperVal: upperVal,
+      ogLowerVal: lowerRoundVal,
+      ogUpperVal: upperRoundVal,
     })
   }
 
   handleChange = (e, value) => {
     debug('handleChange()', value)
-    _.invoke(this.props, 'onChange', e, { ...this.props, value })
+    _.invoke(this.props, 'onChange', e, value)
   }
 
-  onDrag = (e, d) => {
+  handleDrag = (e, d) => {
     const { ogLowerVal, ogUpperVal } = this.state
     const { lastX } = d
     const { classList } = d.node
@@ -185,24 +269,69 @@ export default class Slider extends Component {
       (classList.contains(UPPER_BOUND) && val < ogLowerVal) ||
       (classList.contains(LOWER_BOUND) && val < ogUpperVal)
     ) {
+      const rawVals = {
+        lowerRawVal: val,
+        upperRawVal: classList.contains(UPPER_BOUND) ? ogLowerVal : ogUpperVal,
+      }
+      const roundVals = {
+        lowerRoundVal: this.roundToIncrement(rawVals.lowerRawVal),
+        upperRoundVal: this.roundToIncrement(rawVals.upperRawVal),
+      }
+      this.handleChange(e, [roundVals.lowerRoundVal, roundVals.upperRoundVal])
       this.setState({
-        lowerVal: val,
-        upperVal: classList.contains(UPPER_BOUND) ? ogLowerVal : ogUpperVal,
+        ...rawVals,
+        ...roundVals,
       })
     } else {
+      const rawVals = {
+        lowerRawVal: classList.contains(LOWER_BOUND) ? ogUpperVal : ogLowerVal,
+        upperRawVal: val,
+      }
+      const roundVals = {
+        lowerRoundVal: this.roundToIncrement(rawVals.lowerRawVal),
+        upperRoundVal: this.roundToIncrement(rawVals.upperRawVal),
+      }
+      this.handleChange(e, [roundVals.lowerRoundVal, roundVals.upperRoundVal])
       this.setState({
-        upperVal: val,
-        lowerVal: classList.contains(LOWER_BOUND) ? ogUpperVal : ogLowerVal,
+        ...rawVals,
+        ...roundVals,
       })
     }
   }
 
+  /** isStop is treated as a boolean flag to trigger incrementRounding */
+  onDrag = (e, d) => {
+    debug('onDrag: ', d)
+    this.handleDrag(e, d)
+  }
+
   onStop = (e, d) => {
-    this.onDrag(e, d)
-    this.setState({
-      ogUpperVal: null,
+    debug('onStop: ', d)
+
+    const { lowerRoundVal, upperRoundVal } = this.state
+
+    const returnVals = [lowerRoundVal, upperRoundVal]
+
+    const nullVals = {
+      lowerRawVal: null,
+      upperRawVal: null,
       ogLowerVal: null,
-    })
+      ogUpperVal: null,
+    }
+
+    this.handleChange(e, returnVals)
+
+    this.setState(nullVals)
+  }
+
+  roundToIncrement = (val) => {
+    const { min, increment } = this.props
+
+    if (!increment) return val
+
+    const offset = floatModDiv(+min, +increment)
+
+    return precisionRound((val - offset) / increment, 0) * increment + offset
   }
 
   // internal method
@@ -210,26 +339,29 @@ export default class Slider extends Component {
     if (!this.innerNode) throw Error('this is being called before DOM is mounted!')
 
     const { width } = this.innerNode.getClientRects()[0]
+    const { reversed } = this.props
     const boundPx = constrainNum(px, 0, width)
 
-    return Math.round(1000 * boundPx / width) / 10
+    const ret = precisionRound(1000 * boundPx / width / 10)
+
+    return reversed ? invertPct(ret) : ret
   }
 
   scalePxToVal = (px) => {
-    const { min, max, step } = this.props
+    const { min, max } = this.props
     const pct = this.scalePxToPct(px)
-    // const difference = step == 0 ? value : Math.round(value / step) * step
-
     const val = (max - min) * (pct / 100) + min
 
-    return step === 0 ? precisionRound(val, 0) : precisionRound(val / step, 0) * step
+    return precisionRound(val)
   }
 
   scaleValToPct = (val) => {
-    const { min, max, precision } = this.props
+    const { min, max, reversed } = this.props
     const boundVal = constrainNum(val, min, max)
 
-    return precisionRound(100 * ((boundVal - min) / (max - min)), precision)
+    const ret = precisionRound(100 * ((boundVal - min) / (max - min)))
+
+    return reversed ? invertPct(ret) : ret
   }
 
   render() {
@@ -243,22 +375,34 @@ export default class Slider extends Component {
       // max,
       // min,
       // name,
-      // smooth,
+      lower,
+      upper,
+      lowerStart,
+      upperStart,
+      reversed,
+      smooth,
       // start,
-      // step,
+      // increment,
       // tabIndex,
     } = this.props
-    const { lowerVal, upperVal } = this.state
+    const { lowerRawVal, upperRawVal, lowerRoundVal, upperRoundVal } = this.state
 
     const handleSize = '1em'
 
-    const lowerPct = formatPctStr(this.scaleValToPct(lowerVal))
-    const upperPct = formatPctStr(this.scaleValToPct(upperVal))
-    const fillLeft = lowerPct
-    const fillRight = formatPctStr(100 - this.scaleValToPct(upperVal))
+    const lowerVal = smooth && !!lowerRawVal ? lowerRawVal : lowerRoundVal
+    const upperVal = smooth && !!upperRawVal ? upperRawVal : upperRoundVal
 
-    const lowerHandleLeft = `calc(${lowerPct} - calc(${handleSize} / 2))`
-    const upperHandleLeft = `calc(${upperPct} - calc(${handleSize} / 2))`
+    const lv = _.isNumber(lower) ? lower : lowerVal
+    const uv = _.isNumber(upper) ? upper : upperVal
+
+    const lowerPct = this.scaleValToPct(lv)
+    const upperPct = this.scaleValToPct(uv)
+
+    const fillLeft = `${reversed ? upperPct : lowerPct}%`
+    const fillRight = `${reversed ? invertPct(lowerPct) : invertPct(upperPct)}%`
+
+    const lowerHandleLeft = `calc(${lowerPct}% - calc(${handleSize} / 2))`
+    const upperHandleLeft = `calc(${upperPct}% - calc(${handleSize} / 2))`
 
     const classes = cx(
       'ui',
@@ -269,9 +413,13 @@ export default class Slider extends Component {
       'slider',
       className,
     )
+
     const unhandled = getUnhandledProps(Slider, this.props)
     const ElementType = getElementType(Slider, this.props)
     const [htmlInputProps, rest] = partitionHTMLProps(unhandled, { htmlProps: htmlInputAttrs })
+
+    const l = 'left'
+    const r = 'right'
 
     const lowerHandle = () => (
       <DraggableCore
@@ -283,7 +431,8 @@ export default class Slider extends Component {
         <div
           className={`${LOWER_BOUND} thumb`}
           style={{
-            left: lowerHandleLeft,
+            [l]: lowerHandleLeft,
+            [r]: 'auto',
           }}
         />
       </DraggableCore>
@@ -298,7 +447,8 @@ export default class Slider extends Component {
         <div
           className={`${UPPER_BOUND} thumb`}
           style={{
-            left: upperHandleLeft,
+            [l]: upperHandleLeft,
+            [r]: 'auto',
           }}
         />
       </DraggableCore>
@@ -308,11 +458,12 @@ export default class Slider extends Component {
       <div
         className='track-fill'
         style={{
-          left: fillLeft,
-          right: fillRight,
+          [l]: fillLeft,
+          [r]: fillRight,
         }}
       />
     )
+
     const inner = () => (
       <div
         ref={(n) => {
@@ -322,8 +473,8 @@ export default class Slider extends Component {
       >
         {track()}
         {trackFill()}
-        {lowerHandle()}
-        {upperHandle()}
+        {(lowerStart || lower) && lowerHandle()}
+        {(upperStart || upper) && upperHandle()}
       </div>
     )
 
